7b8b680c8c7f524f31a337c8f3019a74ea8008ad
Leonardo Narciso Oliveira do Vale <leonardo.vale@marquise.com.br>
2019-03-26 17:18:45 -0300

    Putting @CrossOrigin annotation for integration with angular.

--- src/main/java/com/leonardonarciso/springbootangular/controller/CarController.java
+++ src/main/java/com/leonardonarciso/springbootangular/controller/CarController.java
@@ -4,7 +4,9 @@ import java.util.Collection;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +21,8 @@ public class CarController {
 	private CarRepository carRepository;
 
 	@GetMapping("/list")
-	public Collection<Car> listOfCars(){
+	@CrossOrigin(origins = "http://localhost:4200")
+	public Collection<Car> listOfCars() {
 		return carRepository.findAll().stream().filter(this::isCool).collect(Collectors.toList());
 	}
 
